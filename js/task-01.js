d152384b64a322db2fec14930584ddad87998de8
HlibTrebin <109166438+HlibTrebin@users.noreply.github.com>
2022-10-11 13:12:10 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listRef = document.querySelector("#categories");
+console.log(listRef);
+
+const listItemsRef = document.querySelectorAll(".item");
+console.log(listItemsRef);
+
+console.log(`Number of categories: ${listItemsRef.length}`);
