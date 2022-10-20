9197909803c086d898a152600b657a1dce19008d
HlibTrebin <109166438+HlibTrebin@users.noreply.github.com>
2022-10-20 15:27:46 +0300

    sandbox update

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,13 @@
-const result = {};
-
 const inputRef = document.querySelector("#validation-input");
-// console.dir(inputRef);
-// console.log(inputRef.dataset.length);
-// console.log(inputRef.attributes);
 
-inputRef.addEventListener("blur", () => {
-  inputRef.classList.add("#validation-input");
-  console.log(inputRef.classList);
-  if (inputRef.value.length < inputRef.dataset.length) {
-    console.log(`значение инпута: ${inputRef.value}`);
-    console.log(`длина инпута: ${inputRef.value.length}`);
-    inputRef.classList.replace("#validation-input", "#validation-input.valid");
-    return console.log("no");
+const inputValidation = (event) => {
+  if (event.currentTarget.value.length >= inputRef.dataset.length) {
+    inputRef.classList.remove("invalid");
+    return inputRef.classList.add("valid");
   }
+  event.currentTarget.value.length < inputRef.dataset.length;
+  inputRef.classList.remove("valid");
+  return inputRef.classList.add("invalid");
+};
 
-  console.log(`значение инпута: ${inputRef.value}`);
-  console.log(`длина инпута: ${inputRef.value.length}`);
-  inputRef.classList.replace("#validation-input", "#validation-input.invalid");
-  return console.log("yes");
-});
+inputRef.addEventListener("blur", inputValidation);
